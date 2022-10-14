60669b4273eee8dcc698f5780a7111e187cb85f7
MathurJi <anileshjjssacademy84@gmail.com>
2022-10-15 00:05:11 +0530

    add the middleware

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,10 +2,16 @@ import React from 'react';
 
 class Navbar extends React.Component{
   render(){
+
+    handleAdd
+
+    handleChange=()=>
+
+
     return (
         <div className="nav">
             <div className='search-container'>
-                <input/>
+                <input onChange={this.handleChange}/>
                 <button id="search-btn">Search</button>
 
             </div>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {createStore } from 'redux';
+import {applyMiddleware, createStore } from 'redux';
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers';
 
-const store=createStore(rootReducer);
+
+//function logger(obj,next,action)
+// const logger =function({dispatch,getState}){
+//     return function(next){
+//         return function(action){
+//             //middleware
+//             console.log('ACCTION_TYPE',action.type);
+//             next(action);
+//         }
+//     }
+// }
+
+
+const logger=({dispatch,getState})=>(next)=>(action)=>{
+    //logger code 
+    console.log('ACCTION_TYPE',action.type);
+    next(action);
+}
+
+const store=createStore(rootReducer,applyMiddleware(logger));
 console.log('store',store);
 // console.log('BEFORE STATE',store.getState());
 
